a56dcdc84a42ffeb7809c8129d6e8af8f7eaa808
Alex Kalin <krisfromheaven@gmail.com>
2021-11-10 15:15:35 +0300

    share

--- data.js
+++ data.js
@@ -2,8 +2,8 @@ const data = {
   description: 'Her painting serves as the positive double of reality',
   share: {
     default_img: require('./public/share.png'),
-    title: 'Вокруг – шумный город',
-    description: 'Отключи источники звука, чтобы насладиться любимыми треками с наушниками JBL.',
+    title: 'Zoe Sever',
+    description: 'follow form fun',
   },
 }
 
