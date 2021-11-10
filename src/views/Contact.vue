a5d212b024a721702a111953eb4f8d8e2cf55085
Alex Kalin <krisfromheaven@gmail.com>
2021-11-10 13:07:44 +0300

    format tel

--- src/views/Contact.vue
+++ src/views/Contact.vue
@@ -6,7 +6,7 @@
           h1.title.contact__title Contacts
           .contact__info-row
             a.contact__info-link(href='mailto:severzoe@gmail.com' target='_blank') severzoe@gmail.com
-            a.contact__info-link(href='tel:+972548084289' target='_blank') (+972) 548084289
+            a.contact__info-link(href='tel:+972548084289' target='_blank') +972 54-808-4289
         .contact__img-wrapper
           img.contact__img(src='@/assets/img/contact-img.jpg')
 </template>
