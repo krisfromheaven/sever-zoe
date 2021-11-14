c5ebeae43940e1594cd5e3c978f37ac7c7f4f985
Alex Kalin <krisfromheaven@gmail.com>
2021-11-14 11:24:23 +0300

    og meta

--- src/App.vue
+++ src/App.vue
@@ -18,13 +18,13 @@ export default {
   metaInfo() {
     return {
       title: 'Zoe Sever',
-      meta: [
-        { charset: 'utf-8' },
-        { name: 'description', content: 'Her painting serves as the positive double of reality'},
-        { property: 'og:title', content: 'Zoe Sever'},
-        { property: 'og:url', content: 'https://zoesever.com/'},
-        { property: 'og:image', content: 'https://firebasestorage.googleapis.com/v0/b/zoe-sever.appspot.com/o/images%2F136-Zoe%20Sever_Old%20Olive%20Tree%20on%20the%20Way%20to%20Jerusalem_oil%20and%20acrylic%20on%20canvas_90x140%20cm_2018.jpg?alt=media&token=6fcef88d-c64f-46b0-b021-7749bd716c79' }
-      ]
+      // meta: [
+      //   { charset: 'utf-8' },
+      //   { name: 'description', content: 'Her painting serves as the positive double of reality'},
+      //   { property: 'og:title', content: 'Zoe Sever'},
+      //   { property: 'og:url', content: 'https://zoesever.com/'},
+      //   { property: 'og:image', content: 'https://firebasestorage.googleapis.com/v0/b/zoe-sever.appspot.com/o/images%2F136-Zoe%20Sever_Old%20Olive%20Tree%20on%20the%20Way%20to%20Jerusalem_oil%20and%20acrylic%20on%20canvas_90x140%20cm_2018.jpg?alt=media&token=6fcef88d-c64f-46b0-b021-7749bd716c79' }
+      // ]
     }
   },
   components: {
