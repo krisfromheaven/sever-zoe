cca422d8008726b7b31ca65cf89411bc1a08f1ae
Alex Kalin <krisfromheaven@gmail.com>
2021-11-15 16:40:43 +0300

    fix share

--- src/components/layout/Footer.vue
+++ src/components/layout/Footer.vue
@@ -2,10 +2,7 @@
   footer.footer
     .container
       .social.footer__social
-        //a.footer__social-link(href='https://www.facebook.com/Zoe-Sever-Artist-1475026252748227' target='_blank')
-        //  Facebook.footer__social-icon
-        //a.footer__social-link(href='mailto: severzoe@gmail.com' target='_blank')
-        //  Gmail.footer__social-icon
+
         Share
       span.footer__copy  Zoe Sever, 2021 / All rights reserved
     button.footer__btn(type='button' @click.prevent='$router.push("/auth")')
@@ -20,7 +17,7 @@ import Logo from '@/assets/img/logo.svg'
 
 export default {
   name: 'Footer',
-  components: { Facebook, Gmail, Logo,Share },
+  components: { Facebook, Gmail, Logo, Share }
 }
 </script>
 
